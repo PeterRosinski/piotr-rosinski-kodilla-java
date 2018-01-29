4baf2aec10f281bae7ba2ced462c329979d5e86c
PeterRosinski <rosinski.piotr@gmail.com>
2018-01-29 11:50:49 +0100

    FirstCommit

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+//rzeczowniki
+//paczka plikow
+//MailManager
+//Watcher
+//Workflow
+//SQL info
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            WatchService watcher = FileSystems.getDefault().newWatchService();
+            Path dir = Paths.get("C:/temp/test");
+            dir.register(watcher, ENTRY_CREATE);
+
+            System.out.println("Watch Service registered for dir: " + dir.getFileName());
+
+            while (true) {
+                WatchKey key;
+                try {
+                    key = watcher.take();
+                } catch (InterruptedException ex) {
+                    return;
+                }
+
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    WatchEvent.Kind<?> kind = event.kind();
+
+                    @SuppressWarnings("unchecked")
+                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
+                    Path fileName = ev.context();
+
+                    System.out.println(kind.name() + ": " + fileName);
+
+                    String str = fileName.toString();
+                    System.out.println(str.substring(str.length()-4));
+
+                }
+
+                boolean valid = key.reset();
+                if (!valid) {
+                    break;
+                }
+            }
+
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
